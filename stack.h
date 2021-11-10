5c37b894ee4ef22bffb872b6256250afb785cf65
AlekseiSeverin03 <89187267+AlekseiSeverin03@users.noreply.github.com>
2021-11-10 18:56:08 +0300

    Add files via upload

--- stack.h
+++ stack.h
@@ -6,31 +6,31 @@
 
 enum OUTPUT_FUNCTION
 {
-	NOT_OKEY              = 20,
-	NULL_STACK_PTR        = 21,
-	NULL_DATA_PTR         = 22,
-	DATA_PTR_IS_ERR_PTR   = 23,
-	WRONG_HASH            = 24,
-	BAD_HASH              = 25,
+    NOT_OKEY              = 20,
+    NULL_STACK_PTR        = 21,
+    NULL_DATA_PTR         = 22,
+    DATA_PTR_IS_ERR_PTR   = 23,
+    WRONG_HASH            = 24,
+    BAD_HASH              = 25,
 
-	OKEY                  = 0,
-	EMP_STACK             = 1,
-	ERR_ALLOCATION_MEMORY = 2,
+    OKEY                  = 0,
+    EMP_STACK             = 1,
+    ERR_ALLOCATION_MEMORY = 2,
 
-	
+    
 };
 
 enum ERR_VALUE
 {
-	ERR_PTR = 15,
+    ERR_PTR = 15,
 };
 
 
 enum OUTPUT_fMAIN
 {
-	OK = 0,
-	ERR_RESIZE = 71,
-	ERR_MEM = 44,
+    OK = 0,
+    ERR_RESIZE = 71,
+    ERR_MEM = 44,
 };
 
 
@@ -41,16 +41,16 @@ typedef int    hash_t;
 
 typedef struct Stack
 {
-	int canary1;
+    int canary1;
 
-	hash_t hash_stack;
-	hash_t hash_data;
+    hash_t hash_stack;
+    hash_t hash_data;
 
-	int capacity;
-	int size;
-	elem_t *data;
+    int capacity;
+    int size;
+    elem_t *data;
 
-	int canary2;
+    int canary2;
 }                       Stack;
 
 
@@ -58,7 +58,7 @@ typedef struct Stack
        void  StackDtor             (      Stack *);
        int   StackPush             (      Stack *, elem_t);
        int   StackPop              (      Stack *, elem_t *);
-	   void  StackCheck            (      Stack *);
+       void  StackCheck            (      Stack *);
        int   StackOk               (      Stack *);
        void  StackDump             (const Stack *, const char *, const int);
 static int   StackResize           (      Stack *, int);
@@ -70,14 +70,14 @@ static hash_t HashVar              (const void *, size_t, int);
        void  CalcHashes            (      Stack *);
 
 
-
-const hash_t 			  HASH_COEF 		 = 1;
-const int 				  INITIAL_CAPACITY 	 = 16;
-const int 				  SPACE_FACTOR 		 = 2;
-const int 				  REDUCTION_FACTOR 	 = 4;
-const int 				  SIZE_CANARY  		 = sizeof (unsigned long long);            // canary for Stack.data
-const int 				  MAGIC_NUM    		 = 15;
-const int 				  POISON       		 = -666;
+const int                 LITTLE_N_ELEMENT   = 5;
+const hash_t              HASH_COEF          = 1;
+const int                 INITIAL_CAPACITY   = 16;
+const int                 SPACE_FACTOR       = 2;
+const int                 REDUCTION_FACTOR   = 4;
+const int                 SIZE_CANARY        = sizeof (unsigned long long);            // canary for Stack.data
+const int                 MAGIC_NUM          = 15;
+const int                 POISON             = -666;
 const unsigned long long  VALUE_DATA_CANARY  = 0xFEE1FE11;
 const unsigned long long  VALUE_STACK_CANARY = 0xC0FFEE;
 
@@ -92,45 +92,45 @@ extern FILE *LOG_FILE;
 
 #define STACK_DUMP(wtf) StackDump((wtf), __PRETTY_FUNCTION__, __LINE__);
 
-#define STACK_CTOR(stack_ptr)      													  \
-{                                  													  \
-	if (StackCtor (stack_ptr) == ERR_ALLOCATION_MEMORY)								  \
-	{																				  \
-		fprintf (LOG_FILE, "\nError allocation memory in function \"StackCtor\"\n");  \
-																					  \
-		CLOSE_LOG_FILE ();															  \
-		return ERR_MEM;										 						  \
-	}																				  \
+#define STACK_CTOR(stack_ptr)                                                         \
+{                                                                                     \
+    if (StackCtor (stack_ptr) == ERR_ALLOCATION_MEMORY)                               \
+    {                                                                                 \
+        fprintf (LOG_FILE, "\nError allocation memory in function \"StackCtor\"\n");  \
+                                                                                      \
+        CLOSE_LOG_FILE ();                                                            \
+        return ERR_MEM;                                                               \
+    }                                                                                 \
 }
 
-#define STACK_PUSH(stack_ptr, value)  												  \
-{                                             										  \
-	if (StackPush (stack_ptr, value) == ERR_ALLOCATION_MEMORY)    					  \
-	{																				  \
-		fprintf (LOG_FILE, "\nError allocation memory in function \"StackPush\"\n");  \
-																					  \
-		CLOSE_LOG_FILE (); 															  \
-		return ERR_MEM;																  \
-	}																				  \
+#define STACK_PUSH(stack_ptr, value)                                                  \
+{                                                                                     \
+    if (StackPush (stack_ptr, value) == ERR_ALLOCATION_MEMORY)                        \
+    {                                                                                 \
+        fprintf (LOG_FILE, "\nError allocation memory in function \"StackPush\"\n");  \
+                                                                                      \
+        CLOSE_LOG_FILE ();                                                            \
+        return ERR_MEM;                                                               \
+    }                                                                                 \
 }
 
-#define STACK_POP(stack_ptr, ptr_place)			                                    \
+#define STACK_POP(stack_ptr, ptr_place)                                             \
 {                                                                                   \
-	if (StackPop (stack_ptr, ptr_place) == EMP_STACK)                               \
-	{                                                                               \
-		fprintf (LOG_FILE, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); \
-		fprintf (LOG_FILE, "ERROR in line %d.\n", __LINE__);                        \
-		fprintf (LOG_FILE, "Empty stack!\n");                           		    \
-		fprintf (LOG_FILE, "You can't take anythink from STACK.\n");     			\
-		fprintf (LOG_FILE, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); \
-																					\
-		CLOSE_LOG_FILE();												     		\
-		return EMP_STACK;                                                           \
-	}                                                                               \
+    if (StackPop (stack_ptr, ptr_place) == EMP_STACK)                               \
+    {                                                                               \
+        fprintf (LOG_FILE, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); \
+        fprintf (LOG_FILE, "ERROR in line %d.\n", __LINE__);                        \
+        fprintf (LOG_FILE, "Empty stack!\n");                                       \
+        fprintf (LOG_FILE, "You can't take anythink from STACK.\n");                \
+        fprintf (LOG_FILE, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); \
+                                                                                    \
+        CLOSE_LOG_FILE();                                                           \
+        return EMP_STACK;                                                           \
+    }                                                                               \
 }
 
 #define STACK_DTOR(stack_ptr)   \
-{   							\
-	StackDtor (stack_ptr);      \
+{                               \
+    StackDtor (stack_ptr);      \
 }
 
